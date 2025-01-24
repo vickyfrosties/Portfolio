193b8fbbeb2be0a3edbe0a71d16cc45bb9aea5d6
Sounia <s.mhalla@interface3.be>
2025-01-24 13:29:54 +0100

    Form is set

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,48 +2,71 @@ import "../App.css";
 import "../variables.css";
 import { Element } from "react-scroll";
 import Footer from "../Containers/Footer";
+import { useForm } from "react-hook-form";
 
 const Contact = () => {
+
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <>
       <Element name="Contact">
         <h2>Let's Connect</h2>
-        <section className="container-bip">
+        <section className="formsocial-container">
 
           <section className="socials-section">
-
             <div className="socials">
-              <a href="https://www.linkedin.com/in/sounia-mhalla-dit-aounallah/" target="blank">
-                <img src="/Icons/linkedin.svg" alt="www.linkedin.com/in/sounia-mhalla-dit-aounallah" />
-              </a>
+              <img src="/Icons/linkedin.svg" alt="www.linkedin.com/in/sounia-mhalla-dit-aounallah" />
               <p>LinkedIn</p>
+              <a href="https://www.linkedin.com/in/sounia-mhalla-dit-aounallah/" target="blank">
+                <img id="arrow" src="/Icons/arrow-link.svg" alt="" />
+              </a>
             </div>
 
             <div className="socials">
-              <a href="https://github.com/vickyfrosties" target="blank">
-                <img src="/Icons/github.svg" alt="https://github.com/vickyfrosties" />
-              </a>
+              <img src="/Icons/github.svg" alt="https://github.com/vickyfrosties" />
               <p>GitHub</p>
+              <a href="https://github.com/vickyfrosties" target="blank">
+                <img id="arrow" src="/Icons/arrow-link.svg" alt="" />
+              </a>
             </div>
           </section>
 
+          <form className="form" action="" onSubmit={handleSubmit(onSubmit)}>
+            <div className="input-div">
+              <label htmlFor="">
+                Name
+              </label>
+              <input {...register("name")} type="text" placeholder="Name" />
+            </div>
 
-          <form className="form" action="">
-            <label htmlFor="">
-              Name :
-              <input type="text" placeholder="Name" />
-            </label>
-            <label htmlFor="">
-              Last name :
-              <input type="text" placeholder="Last name" />
-            </label>
-            <label htmlFor="">
-              Email :
-              <input type="email" placeholder="Email" />
-            </label>
+            <div>
+              <label htmlFor="">
+                Last name
+              </label>
+              <input {...register("lastname")} type="text" placeholder="Last name" />
+            </div>
+
+            <div>
+              <label htmlFor="">
+                Email
+              </label>
+              <input {...register("email")} type="email" placeholder="Email" />
+            </div>
+
+            <div>
+              <label htmlFor="">
+                Message
+              </label>
+              <textarea {...register("message")} name="message" id="" placeholder="Feel free to reach out" maxLength={500} />
+            </div>
+
+            <input type="submit" id="submit" />
+
           </form>
         </section>
-      </Element>
+      </Element >
       <Footer />
     </>
   );
