b61d45737aeba3d2716eee70d65d24f9859289c7
Sounia <s.mhalla@interface3.be>
2025-01-24 11:43:40 +0100

    React Hook Form :page_facing_up:

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -8,6 +8,41 @@ const Contact = () => {
     <>
       <Element name="Contact">
         <h2>Let's Connect</h2>
+        <section className="container-bip">
+
+          <section className="socials-section">
+
+            <div className="socials">
+              <a href="https://www.linkedin.com/in/sounia-mhalla-dit-aounallah/" target="blank">
+                <img src="/Icons/linkedin.svg" alt="www.linkedin.com/in/sounia-mhalla-dit-aounallah" />
+              </a>
+              <p>LinkedIn</p>
+            </div>
+
+            <div className="socials">
+              <a href="https://github.com/vickyfrosties" target="blank">
+                <img src="/Icons/github.svg" alt="https://github.com/vickyfrosties" />
+              </a>
+              <p>GitHub</p>
+            </div>
+          </section>
+
+
+          <form className="form" action="">
+            <label htmlFor="">
+              Name :
+              <input type="text" placeholder="Name" />
+            </label>
+            <label htmlFor="">
+              Last name :
+              <input type="text" placeholder="Last name" />
+            </label>
+            <label htmlFor="">
+              Email :
+              <input type="email" placeholder="Email" />
+            </label>
+          </form>
+        </section>
       </Element>
       <Footer />
     </>
