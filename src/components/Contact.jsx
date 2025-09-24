eb177c75f6d9f1abb173a866763679fd4e9fa6a5
vickyfrosties <sounia.mhalla@hotmail.com>
2025-09-24 23:53:57 +0200

    feat: add links and rearrange html elements from button to a

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,118 +2,122 @@ const Contact = () => {
   return (
     <>
       <section className="w-full h-full flex justify-center">
-        <section className="w-3/4 h-3/4 m-auto">
-          <section className="w-90 h-full bg-slate-200 rounded-lg">
-            <div className="h-60">
-              <img
-                src="/public/assets/png/Portrait.png"
-                alt=""
-                className="w-full h-full object-contain"
-              />
-            </div>
+        <section className="w-3/4 h-3/4 m-auto bg-slate-700 rounded-lg rounded-l-2xl flex">
+          <section className="w-90 h-full bg-slate-200 rounded-r-3xl rounded-l-xl">
+            <img
+              src="/assets/png/Portrait.png"
+              alt=""
+              className="w-full h-60 object-cover object-center"
+            />
 
-            <section className="p-3 grid grid-col-3 grid-row-2 gap-x-3 gap-y-7 h-auto ">
-              <div className="bg-zinc-100 flex flex-col justify-center items-center p-3 rounded-lg">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="30"
-                  height="30"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-github-icon lucide-github"
+            <section className="p-3 grid grid-col-3 grid-row-2 gap-x-3 gap-y-7 h-auto">
+              <div>
+                <a
+                  className="flex flex-col justify-center items-center cursor-pointer bg-zinc-100 p-3 rounded-lg h-30"
+                  href="https://github.com/vickyfrosties?tab=repositories"
+                  target="_blank"
                 >
-                  <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
-                  <path d="M9 18c-4.51 2-5-2-7-2" />
-                </svg>
-                <p className="text-center">Check out my GitHub profile.</p>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30"
+                    height="30"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="lucide lucide-github-icon lucide-github"
+                  >
+                    <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+                    <path d="M9 18c-4.51 2-5-2-7-2" />
+                  </svg>
+                  <p className="text-center text-sm">
+                    Check out my GitHub profile.
+                  </p>
+                </a>
               </div>
 
-              <div className="bg-zinc-100 col-start-2 flex flex-col justify-center items-center p-3 rounded-lg">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="30"
-                  height="30"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-linkedin-icon lucide-linkedin"
+              <div className="col-start-2">
+                <a
+                  href="https://www.linkedin.com/in/mhalla-sounia/"
+                  target="_blank"
+                  className="flex flex-col justify-center items-center cursor-pointer bg-zinc-100 p-3 rounded-lg h-30"
                 >
-                  <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
-                  <rect width="4" height="12" x="2" y="9" />
-                  <circle cx="4" cy="4" r="2" />
-                </svg>
-                <p className="text-center">Find me on LinkedIn.</p>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30"
+                    height="30"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="lucide lucide-linkedin-icon lucide-linkedin"
+                  >
+                    <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
+                    <rect width="4" height="12" x="2" y="9" />
+                    <circle cx="4" cy="4" r="2" />
+                  </svg>
+                  <p className="text-center text-sm">Find me on LinkedIn.</p>
+                </a>
               </div>
 
-              <div className="bg-zinc-100 flex flex-col justify-center items-center p-3 rounded-lg">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="30"
-                  height="30"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-mail-icon lucide-mail"
+              <div className="col-span-2">
+                <a
+                  href=""
+                  className="cursor-pointer bg-zinc-100 flex flex-col justify-center items-center p-3 rounded-lg h-30 "
                 >
-                  <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" />
-                  <rect x="2" y="4" width="20" height="16" rx="2" />
-                </svg>
-                <p className="text-center">
-                  I'm always available for new connections.
-                </p>
-              </div>
-
-              <div className="bg-zinc-100 flex flex-col justify-center items-center p-3 rounded-lg">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="30"
-                  height="30"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-file-user-icon lucide-file-user"
-                >
-                  <path d="M14 2v4a2 2 0 0 0 2 2h4" />
-                  <path d="M15 18a3 3 0 1 0-6 0" />
-                  <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7z" />
-                  <circle cx="12" cy="13" r="2" />
-                </svg>
-                <div className="flex">
-                  <p className="text-center">Download my resume</p>
-                  <button className="cursor-pointer flex justify-end items-center">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="20"
-                      height="20"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="lucide lucide-download-icon lucide-download"
-                    >
-                      <path d="M12 15V3" />
-                      <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
-                      <path d="m7 10 5 5 5-5" />
-                    </svg>
-                  </button>
-                </div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30"
+                    height="30"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="lucide lucide-mail-icon lucide-mail"
+                  >
+                    <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" />
+                    <rect x="2" y="4" width="20" height="16" rx="2" />
+                  </svg>
+                  <p className="text-center text-sm">
+                    I'm always available for new connections.
+                  </p>
+                </a>
               </div>
             </section>
+          </section>
+
+          <section className="w-2/4 h-full flex flex-col justify-center items-center m-auto">
+            <div>
+              <h3 className="text-2xl font-semibold bg-linear-65 from-purple-500 to-pink-500 text-center bg-linear-[45deg,#61DAFB_55%,#d7f6fe_100%] bg-clip-text text-transparent">
+                Feel free to reach out - I’d be happy to chat about web
+                development, an opportunity, or simply to connect.
+              </h3>
+            </div>
+
+            <div className="w-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="white"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-quote-icon lucide-quote"
+              >
+                <path d="M16 3a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2 1 1 0 0 1 1 1v1a2 2 0 0 1-2 2 1 1 0 0 0-1 1v2a1 1 0 0 0 1 1 6 6 0 0 0 6-6V5a2 2 0 0 0-2-2z" />
+                <path d="M5 3a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2 1 1 0 0 1 1 1v1a2 2 0 0 1-2 2 1 1 0 0 0-1 1v2a1 1 0 0 0 1 1 6 6 0 0 0 6-6V5a2 2 0 0 0-2-2z" />
+              </svg>
+              <h3 className="text-white">today's quote</h3>
+            </div>
           </section>
         </section>
       </section>
