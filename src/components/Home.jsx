c7cc5cd7d05d13e63d7310a65e978f6be3a5247a
vickyfrosties <sounia.mhalla@hotmail.com>
2025-09-25 15:53:00 +0200

    style: add lighter svg icons, improve link accessibility and add style to homepage layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -100,35 +100,90 @@ const Home = ({ setMode }) => {
 
         <div className="bg-[#C1D1FD] grid col-span-2 rounded-lg p-12 max-2xl:p-7">
           <h3 className="text-2xl font-bold">CONTACT</h3>
-          <p>Let's connect!</p>
-          <section className="flex w-full justify-around">
-            <div className="flex justify-center items-center">
-              <a href="https://linkedin.com/in/mhalla-sounia" target="_blank">
-                <img
-                  className="size-30"
-                  src="/assets/png/text.png"
-                  alt="@Mhalla Sounia on LinkedIn"
-                />
+          <section className="flex w-full justify-between">
+            <div
+              className="flex flex-col justify-center items-center w-40"
+              role="link"
+            >
+              <a
+                href="https://linkedin.com/in/mhalla-sounia"
+                target="_blank"
+                className="bg-slate-200 rounded-lg p-2 flex flex-col justify-center items-center"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="60"
+                  height="60"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-linkedin-icon lucide-linkedin"
+                >
+                  <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
+                  <rect width="4" height="12" x="2" y="9" />
+                  <circle cx="4" cy="4" r="2" />
+                </svg>
+                <p className="text-center w-full pt-2">Find me on LinkedIn.</p>
               </a>
             </div>
 
-            <div className="flex justify-center items-center">
-              <a href="mailto:sounia.mhalla@hotmail.com">
-                <img
-                  className="size-30"
-                  src="/assets/png/mail.png"
-                  alt="mailto:sounia.mhalla@hotmail.com"
-                />
+            <div
+              className="flex flex-col justify-center items-center w-40"
+              role="link"
+            >
+              <a
+                href="mailto:sounia.mhalla@hotmail.com"
+                className="bg-slate-200 rounded-lg p-2 flex flex-col justify-center items-center"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="60"
+                  height="60"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-mail-icon lucide-mail"
+                >
+                  <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" />
+                  <rect x="2" y="4" width="20" height="16" rx="2" />
+                </svg>
+                <p className="text-center w-full pt-2">
+                  Feel free to reach out.
+                </p>
               </a>
             </div>
 
-            <div className="flex justify-center items-center">
-              <a href="https://github.com/vickyfrosties" target="_blank">
-                <img
-                  className="size-30"
-                  src="/assets/png/github.png"
-                  alt="https://github.com/vickyfrosties"
-                />
+            <div className="flex flex-col justify-center items-center w-40">
+              <a
+                href="https://github.com/vickyfrosties"
+                target="_blank"
+                className="bg-slate-200 rounded-lg p-2 flex flex-col justify-center items-center"
+                role="link"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="60"
+                  height="60"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-github-icon lucide-github"
+                >
+                  <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+                  <path d="M9 18c-4.51 2-5-2-7-2" />
+                </svg>
+                <p className="text-center w-full">
+                  Check out my GitHub profile.
+                </p>
               </a>
             </div>
           </section>
