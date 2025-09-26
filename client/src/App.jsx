9555488f2470776aaca54824771ac96650c1afd7
vickyfrosties <sounia.mhalla@hotmail.com>
2025-09-26 13:44:24 +0200

    feat: add browser routing for logical structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,11 @@
-import { useState } from "react";
+import { Outlet } from "react-router";
 import NavBar from "./components/NavBar";
-import Projects from "./components/Projects";
-import About from "./components/About";
-import Home from "./components/Home";
-import Contact from "./components/Contact";
 
 function App() {
-  const [mode, setMode] = useState("Home");
-
   return (
     <>
-      <NavBar mode={mode} setMode={setMode} />
-      {mode === "Home" && <Home setMode={setMode} />}
-      {mode === "Projects" && <Projects />}
-      {mode === "About" && <About setMode={setMode} />}
-      {mode === "Contact" && <Contact />}
+      <NavBar />
+      <Outlet />
     </>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import App from "./App.jsx";
 import "./index.css";
+import Projects from "./components/Projects.jsx";
+import Home from "./components/Home.jsx";
+import Contact from "./components/Contact.jsx";
+import About from "./components/About.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/" element={<App />}>
+          <Route path="home" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
