ddc6e1ed11b2b9b36c389bee7deab60ffd77f3f2
vickyfrosties <sounia.mhalla@hotmail.com>
2025-09-29 15:47:49 +0200

    style: responsive readjustements for Navbar & Footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,9 +2,9 @@ export default function Footer({ className = "" }) {
   return (
     <>
       <footer
-        className={`absolute -bottom-17 left-3/4 -translate-x-1/2 bg-white px-4 text-xs text-black ${className}`}
+        className={`absolute -bottom-17 left-3/4 -translate-x-1/2 bg-white px-4 text-xs text-black ${className} `}
       >
-        <div className="w-90 flex justify-between items-center">
+        <div className="w-90 flex justify-between items-center max-sm:w-full max-sm:h-7">
           <p className="text-sm">2025 - © Sounia Mhalla</p>
           <a
             href="https://github.com/vickyfrosties?tab=repositories"
