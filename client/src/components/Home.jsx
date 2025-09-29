633dd3a34e1bfa4459df1be56086a6d5e47770e2
vickyfrosties <sounia.mhalla@hotmail.com>
2025-09-29 15:47:11 +0200

    feat: add responsive design Home page for small small devices screen

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = ({ className }) => {
   return (
     <>
       <section>
-        <section className="grid grid-cols-3 max-2xl:grid-cols-3 max-2xl:grid-rows-2 2xl:bg-red-500 2xl:grid-rows-2 max-2xl:h-full flex-col min-h-lvh grid-rows-3 p-5 gap-10 w-full max-lg:gap-5 border-b-1 max-md:grid-cols-2 ">
+        <section className="grid grid-cols-3 max-2xl:grid-cols-3 max-2xl:grid-rows-2 2xl:bg-red-500 2xl:grid-rows-2 max-2xl:h-full flex-col min-h-lvh grid-rows-3 p-5 gap-10 w-full max-lg:gap-5 border-b-1 max-md:grid-cols-2 max-sm:flex max-sm:border-0">
           <div className="bg-amber-200 row-span-2 rounded-lg max-2xl:row-start-1 max-2xl:row-end-3 2xl:bg-blue-500 2xl:row-span-2 max-lg:row-span-1 min-h-[250px]">
             <div className="rounded-lg h-full relative">
               <h2 className="text-4xl font-black text-stone-900 absolute ml-5 z-2 top-20 max-2xl:hidden">
@@ -16,7 +16,7 @@ const Home = ({ className }) => {
               </h2>
 
               <div className="absolute z-1 bottom-25 ml-5 mr-5 max-md:-bottom-5 max-md:p-3 max-md:m-auto max-2xl:left-80 max-2xl:static max-2xl:pt-5">
-                <h1 className="text-4xl font-bold max-md:text-2xl max-lg:text-2xl">
+                <h1 className="text-4xl font-bold max-md:text-2xl max-lg:text-2xl max-sm:text-2xl">
                   I'm Mhalla Sounia,
                 </h1>
 
@@ -47,7 +47,7 @@ const Home = ({ className }) => {
                 front-end development about a year ago - and I instantly loved
                 it.
               </p>
-              <p className="text-xl mt-5 max-lg:hidden">
+              <p className="text-xl mt-5 max-lg:hidden max-sm:block">
                 Today, I design and build web interfaces that feeds my learning,
                 spark my creativity, and challenge me to grow. My world? A mix
                 of creativity, curiosity and a constant desire to improve.
@@ -78,7 +78,7 @@ const Home = ({ className }) => {
                 features, refine the code, and apply what I learn along the way.
               </p>
 
-              <p className="text-xl h-30 max-lg:hidden">
+              <p className="text-xl h-30 max-lg:hidden max-sm:block">
                 This helps me build more complete and long-lasting projects
                 while I keep strengthening my skills.
               </p>
@@ -98,7 +98,7 @@ const Home = ({ className }) => {
             <h3 className="text-2xl text-white font-bold">CONTACT</h3>
             <section className="flex w-full justify-between max-md:justify-between">
               <div
-                className="flex flex-col justify-center items-center w-50 "
+                className="flex flex-col justify-center items-center w-50 max-sm:w-20"
                 role="link"
               >
                 <a
@@ -123,19 +123,19 @@ const Home = ({ className }) => {
                     <rect width="4" height="12" x="2" y="9" />
                     <circle cx="4" cy="4" r="2" />
                   </svg>
-                  <p className="text-center w-full pt-2">
+                  <p className="text-center w-full pt-2 max-sm:hidden">
                     Find me on LinkedIn.
                   </p>
                 </a>
               </div>
 
               <div
-                className="flex flex-col justify-center items-center w-50 "
+                className="flex flex-col justify-center items-center w-50 max-sm:w-20"
                 role="link"
               >
                 <a
                   href="mailto:sounia.mhalla@hotmail.com"
-                  className="bg-slate-200 rounded-lg p-2 flex flex-col justify-center items-center max-md:h-30"
+                  className="bg-slate-200 rounded-lg p-2 flex flex-col justify-center items-center max-md:h-30 max-sm:w-full"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -153,17 +153,17 @@ const Home = ({ className }) => {
                     <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" />
                     <rect x="2" y="4" width="20" height="16" rx="2" />
                   </svg>
-                  <p className="text-center w-full pt-2">
+                  <p className="text-center w-full pt-2 max-sm:hidden">
                     Feel free to reach out.
                   </p>
                 </a>
               </div>
 
-              <div className="flex flex-col justify-center items-center w-50 ">
+              <div className="flex flex-col justify-center items-center w-50 max-sm:w-20">
                 <a
                   href="https://github.com/vickyfrosties?tab=repositories"
                   target="_blank"
-                  className="bg-slate-200 rounded-lg p-2 flex flex-col justify-center items-center max-md:h-30"
+                  className="bg-slate-200 rounded-lg p-2 flex flex-col justify-center items-center max-md:h-30 max-sm:w-full"
                   role="link"
                 >
                   <svg
@@ -182,7 +182,7 @@ const Home = ({ className }) => {
                     <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
                     <path d="M9 18c-4.51 2-5-2-7-2" />
                   </svg>
-                  <p className="text-center w-full">
+                  <p className="text-center w-full max-sm:hidden">
                     Check out my GitHub profile.
                   </p>
                 </a>
@@ -191,7 +191,7 @@ const Home = ({ className }) => {
           </div>
         </section>
       </section>
-      <Footer className="max-lg:-bottom-37 max-md:-bottom-117 max-md:left-2/3" />
+      <Footer className="max-lg:-bottom-37 max-md:-bottom-117 max-md:left-2/3 max-sm:fixed max-sm:w-full max-sm:left-1/2 " />
     </>
   );
 };
