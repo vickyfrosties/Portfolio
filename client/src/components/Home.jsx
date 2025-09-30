23845367085b13d4ba9cfd402d1fcc775d9dd36b
vickyfrosties <sounia.mhalla@hotmail.com>
2025-09-30 11:36:14 +0200

    feat: add custom breakpoints for small small screen sizes and Home page adjustements

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = ({ className }) => {
   return (
     <>
       <section>
-        <section className="grid grid-cols-3 max-2xl:grid-cols-3 max-2xl:grid-rows-2 2xl:bg-red-500 2xl:grid-rows-2 max-2xl:h-full flex-col min-h-lvh grid-rows-3 p-5 gap-10 w-full max-lg:gap-5 border-b-1 max-md:grid-cols-2 max-sm:flex max-sm:border-0 max-md:border-0 ">
+        <section className="grid grid-cols-3 max-2xl:grid-cols-3 max-2xl:grid-rows-2 2xl:bg-red-500 2xl:grid-rows-2 max-2xl:h-full flex-col min-h-lvh grid-rows-3 p-5 gap-10 w-full max-lg:gap-5 border-b-1 max-md:grid-cols-2 max-sm:flex max-sm:border-0 max-md:border-0">
           <div className="bg-amber-200 row-span-2 rounded-lg max-2xl:row-start-1 max-2xl:row-end-3 2xl:bg-blue-500 2xl:row-span-2 max-lg:row-span-1 min-h-[250px]">
             <div className="rounded-lg h-full relative">
               <h2 className="text-4xl font-black text-stone-900 absolute ml-5 z-2 top-20 max-2xl:hidden">
@@ -42,7 +42,7 @@ const Home = ({ className }) => {
           <div className="bg-emerald-200 rounded-lg p-12 flex flex-col justify-between max-2xl:col-start-2 max-2xl:row-start-1 max-2xl:row-end-3 max-2xl:p-7 max-lg:row-span-1 max-md:min-h-64">
             <div>
               <h3 className="text-2xl font-bold">ABOUT</h3>
-              <p className="text-xl mt-5">
+              <p className="text-xl mt-5 max-xs:hidden">
                 I'm passionate about creating web interfaces. I discovered
                 front-end development about a year ago - and I instantly loved
                 it.
@@ -71,14 +71,14 @@ const Home = ({ className }) => {
           <div className="bg-amber-200 grid row-span-2 rounded-lg p-12 max-2xl:col-start-3 max-2xl:p-7 max-lg:col-start-1 max-md:col-start-2 max-md:row-span-1 max-md:min-h-64">
             <h3 className="text-2xl font-bold h-10">PROJECTS</h3>
 
-            <div className="flex flex-col justify-between h-90">
+            <div className="flex flex-col justify-between h-90  max-xs:h-auto">
               <p className="text-xl">
                 Most of my projects are full-stack applications that I keep
                 improving over time. I enjoy revisiting them to add new
                 features, refine the code, and apply what I learn along the way.
               </p>
 
-              <p className="text-xl h-30 max-lg:hidden max-md:block max-sm:block">
+              <p className="text-xl h-30 max-lg:hidden max-md:block max-sm:block max-xs:hidden">
                 This helps me build more complete and long-lasting projects
                 while I keep strengthening my skills.
               </p>
@@ -94,17 +94,17 @@ const Home = ({ className }) => {
             </div>
           </div>
 
-          <div className="bg-orange-800 grid col-span-2 rounded-lg p-12 max-2xl:p-7 max-lg:h-1/2 max-lg:flex max-lg:flex-col max-lg:justify-between max-md:min-h-64 max-md:justify-between">
+          <div className="bg-orange-800 grid col-span-2 rounded-lg p-12 max-2xl:p-7 max-lg:h-1/2 max-lg:flex max-lg:flex-col max-lg:justify-between max-md:min-h-64">
             <h3 className="text-2xl text-white font-bold">CONTACT</h3>
-            <section className="flex w-full justify-between max-md:justify-between max-sm:justify-around">
+            <section className="flex w-full justify-between max-md:justify-between max-sm:justify-around max-xs:flex max-xs:flex-col max-xs:gap-3 max-xs:justify-between">
               <div
-                className="flex flex-col justify-center items-center w-50 max-sm:w-20"
+                className="flex flex-col justify-center items-center w-50 max-sm:w-20 max-xs:w-full max-xs:h-20"
                 role="link"
               >
                 <a
                   href="https://linkedin.com/in/mhalla-sounia"
                   target="_blank"
-                  className="bg-slate-200 rounded-lg p-2 flex flex-col justify-center items-center max-md:h-30 max-md:w-full"
+                  className="bg-slate-200 rounded-lg p-2 flex flex-col justify-center items-center max-md:h-30 max-md:w-full max-xs:flex-row"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -123,19 +123,19 @@ const Home = ({ className }) => {
                     <rect width="4" height="12" x="2" y="9" />
                     <circle cx="4" cy="4" r="2" />
                   </svg>
-                  <p className="text-center w-full pt-2 max-sm:hidden">
+                  <p className="text-center w-full pt-2 max-sm:hidden max-xs:block">
                     Find me on LinkedIn.
                   </p>
                 </a>
               </div>
 
               <div
-                className="flex flex-col justify-center items-center w-50 max-sm:w-20"
+                className="flex flex-col justify-center items-center w-50 max-sm:w-20 max-xs:w-full max-xs:h-20"
                 role="link"
               >
                 <a
                   href="mailto:sounia.mhalla@hotmail.com"
-                  className="bg-slate-200 rounded-lg p-2 flex flex-col justify-center items-center max-md:h-30 max-sm:w-full"
+                  className="bg-slate-200 rounded-lg p-2 flex flex-col justify-center items-center max-md:h-30 max-sm:w-full max-xs:flex-row"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -153,17 +153,17 @@ const Home = ({ className }) => {
                     <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" />
                     <rect x="2" y="4" width="20" height="16" rx="2" />
                   </svg>
-                  <p className="text-center w-full pt-2 max-sm:hidden">
+                  <p className="text-center w-full pt-2 max-sm:hidden max-xs:block">
                     Feel free to reach out.
                   </p>
                 </a>
               </div>
 
-              <div className="flex flex-col justify-center items-center w-50 max-sm:w-20">
+              <div className="flex flex-col justify-center items-center w-50 max-sm:w-20 max-xs:w-full max-xs:h-20">
                 <a
                   href="https://github.com/vickyfrosties?tab=repositories"
                   target="_blank"
-                  className="bg-slate-200 rounded-lg p-2 flex flex-col justify-center items-center max-md:h-30 max-sm:w-full"
+                  className="bg-slate-200 rounded-lg p-2 flex flex-col justify-center items-center max-md:h-30 max-sm:w-full max-xs:flex-row"
                   role="link"
                 >
                   <svg
@@ -182,7 +182,7 @@ const Home = ({ className }) => {
                     <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
                     <path d="M9 18c-4.51 2-5-2-7-2" />
                   </svg>
-                  <p className="text-center w-full max-sm:hidden">
+                  <p className="text-center w-full max-sm:hidden max-xs:block">
                     Check out my GitHub profile.
                   </p>
                 </a>
